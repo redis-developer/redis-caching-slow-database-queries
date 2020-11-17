99705d58c180aec34766cff8f9faaae2a9f0efdf
Justin Castilla <pxlperfection@gmail.com>
2020-11-17 10:55:02 -0800

    refactored api.js to use async/await, graceful exit

--- nodejs/api.js
+++ nodejs/api.js
@@ -5,74 +5,58 @@ const Redis = require('ioredis')
 const redis = new Redis(constants.redis)
 const axios = require('axios')
 
-const returnEntry = (entry, source, responseTime = false) => {
-  console.log(`Sending from ${source}: `, entry);
-  responseTime && console.log(`Response time: ${responseTime} ms`)
+const cityEndpoint = (city) => api_endpoint = `${constants.endpoint(city)}${constants.key}`
+
+const keyCheck = () =>  {
+  if (!process.env.WEATHER_API_KEY) {
+    console.error(`No Weather API key detected as an ENVIRONMENT VARIABLE.
+    Head to https://openweathermap.org/appid to get a free API key.
+    Then set an environment variable in your command line:
+    $ export WEATHER_API_KEY=<your new api key>`)
+    quit()
+  }
+}
+
+const quit = () => {
+  redis.quit()
   process.exit()
 }
 
 /* Returns a JSON object of the current weather conditions for a given city */
-const getWeather = (city) => {
+const getWeather = async (city) => {
+  keyCheck()
 
   /* Check Redis for cached entry first */
-  redis.get(`weather:${city}`, (err, entry) => {
+  return await redis.get(`weather:${city}`)
+    .then( async entry => {
+      /* If Redis returns a cache hit, */
+      if (entry) {
+        cached_entry = JSON.parse(entry)
+        cached_entry.source = 'cache'
+        /* return the entry */
+        return cached_entry
+      }
 
-    /* If Redis returns a cache hit, */
-    if (entry) {
-
-      /* return the entry */
-      returnEntry(JSON.parse(entry), 'cache')
-
-
-    }
-    /* If Redis returns a chache miss */
-    if(!entry) {
-
-      /* Fetch the data from the API */
-      api_endpoint = `${constants.endpoint(city)}${constants.key}`
-      axios.get(api_endpoint)
-        .then(res => {
-
-          /* Return the database entry */
-          returnEntry(res.data, 'API')
+      /* If Redis returns a cache miss, fetch and return data from the API */
+      return await axios.get(cityEndpoint(city))
+        .then(response => {
 
           /* Add the entry to Redis for next time */
           redis.set(`weather:${city}`, 
-          JSON.stringify(res.data), 
+          JSON.stringify(response.data),
           /* Set an expiry of one hour so weather data is timely */
           'EX', 3600)
+          response.data.source = 'API'
+          return response.data
         })
-    }
-  })
+    })
 }
 
-/* Returns a JSON object of the current weather conditions for a given city, with timing */
-const getWeather_T = (city) => {
-  let startTime = new Date().getTime()
-  redis.get(`weather:${city}`, (err, entry) => {
-    let endTime = new Date().getTime()
-    let responseTime = (endTime - startTime)
-    if (entry) {
-      returnEntry(JSON.parse(entry), 'cache', responseTime)
-      return
-    }
-    if(!entry) {
-      api_endpoint = `${constants.endpoint(city)}${constants.key}`
-      startTime = new Date().getTime()
-      axios.get(api_endpoint)
-        .then(res => {
-          endTime = new Date().getTime()
-          responseTime = (endTime - startTime)
-          returnEntry(res.data, 'API', responseTime)
-          redis.set(`weather:${city}`, 
-          JSON.stringify(res.data), 
-          'EX', 3600)
-          return
-        })
-    }
-  })
-}
-
-( () => {
-  getWeather_T('Oakland')
-})()
+const city = 'Oakland'
+let t0 = new Date().getTime()
+getWeather(city).then(weather => {
+  let t1 = new Date().getTime()
+  weather.responseTime = `${t1-t0}ms`
+  console.log(weather)
+  quit()
+})
